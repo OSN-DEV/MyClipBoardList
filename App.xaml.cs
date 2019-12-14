24a8bbd9da1dc6545790c6a0f9defca47f31d574
OSN-DEV <osndev1973@gmail.com>
2019-12-14 16:42:06 +0900

    二重起動を抑止

--- App.xaml.cs
+++ App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -11,5 +12,28 @@ namespace MyClipBoardList {
     /// App.xaml の相互作用ロジック
     /// </summary>
     public partial class App : Application {
+
+        #region Declaration
+        private Mutex mutex = new Mutex(false, "MyApps");
+        #endregion
+
+        #region Event
+        private void Application_Startup(object sender, StartupEventArgs e) {
+            var name = this.GetType().Assembly.GetName().Name;
+            mutex = new Mutex(false, name);
+            if (!mutex.WaitOne(TimeSpan.Zero, false)) {
+                mutex.Close();
+                this.Shutdown();
+                return;
+            }
+        }
+
+        private void Application_Exit(object sender, ExitEventArgs e) {
+            if (mutex != null) {
+                mutex.ReleaseMutex();
+                mutex.Close();
+            }
+        }
+        #endregion
     }
 }
